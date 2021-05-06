ccb975dc18ad278abfab60c01cc60a9e6105817b
Raj Kishore Sahni <63728002+raj9650sahni@users.noreply.github.com>
2021-05-06 14:41:54 +0530

    Update IAddressBookService.java

--- IAddressBookService.java
+++ IAddressBookService.java
@@ -1,13 +1,27 @@
 package com.bl.addressbook.service;
+
 import java.util.List;
+import java.util.Scanner;
 
 import com.addressbok.model.Person;
 
 public interface IAddressBookService {
 
 	public boolean addPerson(Person person);
+
 	public boolean deletePerson(String name);
+
 	List<Person> getAddressBook();
+
+	public boolean editPerson(String contactName, Scanner scanner);
+
+	public boolean hasPresent(String p);
+	
+	public void showByState(String s);
+	
+	public void showByCity(String c);
+	
+	public int count();
 	
 	
 
